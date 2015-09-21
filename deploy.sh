9eabaea44be0627936eecc2d083131f93e62eca4
borikanes <ooludemi@terpmail.umd.edu>
2015-09-20 23:02:17 -0400

    fixed bash error

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+# screen -X -S 3060.ttys000.34363bd1bda6 quit
+# ps aux |grep -v "grep" | grep "node ./app.js"
+
+ps_output=`ps aux |grep -v "grep" | grep -q "node ./app.js"`
+if $ps_output; then
+  echo "it worked"
+fi
