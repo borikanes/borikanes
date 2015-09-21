aef654a570aa19b809238cf71f8866375cb62134
borikanes <ooludemi@terpmail.umd.edu>
2015-09-20 23:18:56 -0400

    able to extract pid from running process

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 # screen -X -S 3060.ttys000.34363bd1bda6 quit
 # ps aux |grep -v "grep" | grep "node ./app.js"
 
-ps_output=`ps aux |grep -v "grep" | grep -q "node ./app.js"`
-if $ps_output; then
+ps_output=`ps aux |grep -v "grep" | grep "node ./app.js" | awk '{print $6}'`
+echo $ps_output
+if [ $ps_output == "26568" ]; then
   echo "it worked"
 fi
