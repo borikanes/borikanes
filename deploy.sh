74ba305713209a70c7d293e04d65b9ff64ad9ea4
borikanes <ooludemi@terpmail.umd.edu>
2015-09-23 15:42:15 -0400

    added some more stuff to deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,11 @@
 ps_output=`ps aux |grep -v "grep" | grep "node ./app.js" | awk '{print $6}'`
 SCREEN_PROCESS=`screen -list | awk '{print $1}' | grep "tty"`
 
-if [ $SCREEN_PROCESS ]; then
-  screen -X -S $SCREEN_PROCESS quit #kills screen process
-else
+if [ ! $SCREEN_PROCESS ]; then
   echo $(date +%A-%m-%d-%Y_%H-%M-%S) "No screen sessions" >> deploy_log
   exit
+else
+  screen -X -S $SCREEN_PROCESS quit #kills screen process
+  cd ~/Code/borikanes && git pull origin autodeploy-shellscript
+  # /home/pi/Code/borikanes
 fi
-echo "This shouldn't happen"
