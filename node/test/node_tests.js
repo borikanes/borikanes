16cd91b0d84508633e0b15585cf430a6baf5c9d2
borikanes <ooludemi@terpmail.umd.edu>
2015-10-23 11:00:47 -0400

    testing why travis doesnt like res.body

--- node/test/node_tests.js
+++ node/test/node_tests.js
@@ -18,6 +18,7 @@ describe('**************** Node tests ***************', function() {
     it('check /hello exists', function(done){
       request.get('localhost:8080/hello').end(function(err, res){
         expect(res).to.exist;
+        console.log(JSON.stringify(res));
         expect(res.body).to.not.contain("Cannot GET");
         expect(res.body.message).to.contain("welcome");
         done();
