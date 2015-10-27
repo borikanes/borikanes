2256566abbdb60eaa6635126ddefa0492d7783b9
borikanes <ooludemi@terpmail.umd.edu>
2015-10-27 16:25:49 -0400

    omg it worked. i did not start the server; that was the problem

--- node/test/node_tests.js
+++ node/test/node_tests.js
@@ -11,7 +11,6 @@ describe('**************** Node tests ***************', function() {
     it('check root(/) exists', function(done){
       request.get('localhost:8080').end(function(err, res){
         expect(res).to.exist;
-        //console.log('---------->'+JSON.stringify(res));
         done();
       });
     });
@@ -19,7 +18,6 @@ describe('**************** Node tests ***************', function() {
     it('check /hello exists', function(done){
       request.get('localhost:8080/hello').end(function(err, res){
         expect(res).to.exist;
-        console.log('---------->'+JSON.stringify(res));
         expect(res.body).to.not.contain("Cannot GET");
         expect(res.body.message).to.contain("welcome");
         done();
