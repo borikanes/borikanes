1509654ea574ce8aef6c79cd3aa397e3d01c43bb
borikanes <ooludemi@terpmail.umd.edu>
2015-11-23 08:49:19 -0500

    added route to get resume.pdf...also added resume.pdf

--- flask/flask.py
+++ flask/flask.py
@@ -1,7 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, send_file
 
 app = Flask(__name__)
 
 @app.route('/resume')
 def get_resume:
-    
+    with open('/Resume.pdf', 'rb') as pdf:
+        return send_file(pdf, attachment_filename='Resume.pdf')
