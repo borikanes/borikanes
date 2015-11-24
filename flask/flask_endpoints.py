3dffc6cae17c0d9e6f46f79d74e35721f71482b9
borikanes <ooludemi@terpmail.umd.edu>
2015-11-24 11:44:43 -0500

    implemented resume route on flask side

---
+++ flask/flask_endpoints.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, make_response, send_file
+
+app = Flask(__name__)
+
+@app.route('/resume', methods=['GET'])
+def get_resume():
+    file_name = 'Resume.pdf'
+    return send_file(file_name, as_attachment=False, mimetype='application/pdf')
+
+@app.route('/')
+def flask_home():
+    return "Flask is running on borikanes.me"
+
+if __name__ == '__main__':
+    app.run('0.0.0.0',port=8000,debug=True)
