7e16165754f86fd723589765c7f25edd02c0d29f
borikanes <ooludemi@terpmail.umd.edu>
2015-11-29 08:29:28 -0500

    i think it worked but confirming again

--- deploy.sh
+++ deploy.sh
@@ -8,21 +8,21 @@ FIRST_SCREEN_PROCESS=`screen -list | awk '{print $1}' | grep "\.\."  | sed -n 1p
 SECOND_SCREEN_PROCESS=`screen -list | awk '{print $1}' | grep "\.\."  | sed -n 2p` # gets second screen process
 
 if [ ! "$SCREEN_PROCESS" ]; then
-  echo $(date +%A-%m-%d-%Y_%H-%M-%S) "No screen sessions" >> deploy_log
+  echo $(date +%A-%m-%d-%Y_%H-%M-%S) "No screen sessions" >> /home/pi/Code/borikanes/deploy_log
   exit
 else
-  echo "Else statement" >> deploy_log
+  echo $(date +%A-%m-%d-%Y_%H-%M-%S) "Else statement" >> /home/pi/Code/borikanes/deploy_log
   if [ "$FIRST_SCREEN_PROCESS" ]; then
     screen -X -S $FIRST_SCREEN_PROCESS quit #kills first screen process
-    echo "Killed first screen" >> deploy_log
+    echo $(date +%A-%m-%d-%Y_%H-%M-%S) "Killed first screen" >> /home/pi/Code/borikanes/deploy_log
   fi
   if [ "$SECOND_SCREEN_PROCESS" ]; then
     screen -X -S $SECOND_SCREEN_PROCESS quit #kills second screen process
-    echo "Killed second screen" >> deploy_log
+    echo $(date +%A-%m-%d-%Y_%H-%M-%S) "Killed second screen" >> /home/pi/Code/borikanes/deploy_log
   fi
-  echo "Got through" >> deploy_log
+  echo $(date +%A-%m-%d-%Y_%H-%M-%S) "Got through" >> /home/pi/Code/borikanes/deploy_log
   cd /home/pi/Code/borikanes && git pull origin master
   cd /home/pi/Code/borikanes/node && screen -d -m npm start
   cd /home/pi/Code/borikanes/flask && screen -d -m python3.4 flask_endpoints.py
-  echo $(date +%A-%m-%d-%Y_%H-%M-%S) "Deploy Succesfull!" >> deploy_log
+  echo $(date +%A-%m-%d-%Y_%H-%M-%S) "Deploy Succesfull!" >> /home/pi/Code/borikanes/deploy_log
 fi
