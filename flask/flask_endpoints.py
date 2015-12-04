0d38d655edb05c8e4217efc91112bc7744109e8e
borikanes <ooludemi@terpmail.umd.edu>
2015-12-04 09:54:36 -0500

    trying just python script now

--- flask/flask_endpoints.py
+++ flask/flask_endpoints.py
@@ -12,4 +12,4 @@ def flask_home():
     return "Flask is running on borikanes.me"
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0',port=5001,debug=True)
