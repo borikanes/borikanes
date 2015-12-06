9b33815db6bd96f4f1e79f6b0f78f40e6e4c5e0e
borikanes <ooludemi@terpmail.umd.edu>
2015-12-05 22:54:02 -0500

    trying someting different

--- deploy_assist.py
+++ deploy_assist.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 
 os.system("echo $(date +%A-%m-%d-%Y_%H-%M-%S) \"About to kill screen sessions\" >> $BORIKANES_HOME/deploy_log")
-os.system("screen -ls | grep Detached | cut -d. -f1 | awk \'{print $1}\' | xargs kill")
+error_variable = os.popen("screen -ls | grep Detached | cut -d. -f1 | awk \'{print $1}\' | xargs kill")
 os.system("echo $(date +%A-%m-%d-%Y_%H-%M-%S) \"Killed all screen sessions\" >> $BORIKANES_HOME/deploy_log")
 os.system("cd $BORIKANES_HOME && git pull origin master && cd $BORIKANES_HOME/node && screen -d -m npm start && cd $BORIKANES_HOME/flask && screen -d -m python3.4 flask_endpoints.py")
 os.system("echo $(date +%A-%m-%d-%Y_%H-%M-%S) \"Deploy Succesfull!\" >> $BORIKANES_HOME/deploy_log")
