97c6f795bf5b34276e910af3362f757644d8bc30
borikanes <ooludemi@terpmail.umd.edu>
2015-12-07 06:24:40 -0500

    runnning screen sesh as the right user

--- deploy_assist.py
+++ deploy_assist.py
@@ -5,6 +5,7 @@ import subprocess
 # os.system("echo $(date +%A-%m-%d-%Y_%H-%M-%S) \"About to kill screen sessions\" >> $BORIKANES_HOME/deploy_log")
 error_variable = os.popen("screen -ls | grep Detached | cut -d. -f1 | awk \'{print $1}\' | xargs kill")
 # os.system("echo $(date +%A-%m-%d-%Y_%H-%M-%S) \"Killed all screen sessions\" >> $BORIKANES_HOME/deploy_log")
-os.system("cd $BORIKANES_HOME && git pull origin master && cd $BORIKANES_HOME/node && screen -d -m npm start && cd $BORIKANES_HOME/flask && screen -d -m python3.4 flask_endpoints.py")
+os.system("cd $BORIKANES_HOME && git pull origin master")
+os.system("cd $BORIKANES_HOME/node && sudo -u pi screen -d -m npm start && cd $BORIKANES_HOME/flask && sudo -u pi screen -d -m python3.4 flask_endpoints.py")
 # os.system("echo "+restart_variable+" >> $BORIKANES_HOME/deploy_log")
 # os.system("echo $(date +%A-%m-%d-%Y_%H-%M-%S) \"Deploy Succesfull!\" >> $BORIKANES_HOME/deploy_log")
