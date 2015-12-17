4a153098fd3c5e5b8162edb52213617dbfe5e346
borikanes <ooludemi@terpmail.umd.edu>
2015-12-17 00:37:44 -0500

    mistakes were made

--- deploy_assist.py
+++ deploy_assist.py
@@ -15,4 +15,3 @@ error_variable = os.popen("screen -ls | grep Detached | cut -d. -f1 | awk \'{pri
 os.system("cd $BORIKANES_HOME/node && screen -d -m npm start && cd $BORIKANES_HOME/flask && screen -d -m python3.4 flask_endpoints.py")
 # os.system("echo "+restart_variable+" >> $BORIKANES_HOME/deploy_log")
 # os.system("echo $(date +%A-%m-%d-%Y_%H-%M-%S) \"Deploy Succesfull!\" >> $BORIKANES_HOME/deploy_log")
-# This is for testing
