1425100e0a0e1f5221a476e2a41fe36a33d2adbc
borikanes <ooludemi@terpmail.umd.edu>
2016-01-11 17:15:56 -0500

    adding twilio support

--- flask/flask_endpoints.py
+++ flask/flask_endpoints.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, make_response, send_file
+from twilio.rest import TwilioRestClient
+import os
 
 app = Flask(__name__)
+account_sid = os.environ.get('TWILIO_SID_KEY')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
 
 @app.route('/resume', methods=['GET'])
 def get_resume():
@@ -10,6 +14,10 @@ def get_resume():
 @app.route('/flask')
 def flask_home():
     return "Flask is running on borikanes.me"
+
+@app.route('/twilio')
+def post_twilio():
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=5000,debug=True)
